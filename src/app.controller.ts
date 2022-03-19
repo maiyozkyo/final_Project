6f0069f5899109d8304baf387942084f2fe3e170
teebee179 <80611478+teebee179@users.noreply.github.com>
2022-03-20 00:16:44 +0700

    Commit 2
    change header, footer, link

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -35,4 +35,11 @@ export class AppController {
   whyRouter(@Request() req: Request){
     console.log(req);
   }
+
+  @Get('/product/detail')
+  @Render('./User/productDetail')
+  detailRouter(@Request() req: Request){
+    console.log(req);
+  }
 }
+
