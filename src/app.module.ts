c5ddc35075fea45975649f60193a66cfd224c551
maiyozkyo <ekkobuu@gmail.com>
2022-03-20 10:51:52 +0700

    delete file ormconfig

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,18 @@ import { AdminHomeModule } from './admin-home/admin-home.module';
 import { AdminProductModule } from './admin-product/admin-product.module';
 import { AdminLoginModule } from './admin-login/admin-login.module';
 
+
 @Module({
-  imports: [AdminHomeModule, AdminProductModule, AdminLoginModule],
+  imports: [
+    AdminHomeModule,
+    AdminProductModule,
+    AdminLoginModule,
+    
+  ],
   controllers: [AppController],
   providers: [AppService],
+
 })
-export class AppModule {}
+export class AppModule {
+  
+}
