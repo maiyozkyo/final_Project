99769659441a1b4c7b7268b8e30e1018e735c95d
xmax2807 <19127425@student.hcmus.edu.vn>
2022-04-02 23:58:24 +0700

    sign in

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,14 +1,23 @@
-import { Controller, Get, Render, Request, Post } from '@nestjs/common';
+import { Controller, Get, Render,Request, Post, Redirect, Res, Req } from '@nestjs/common';
 import { AppService } from './app.service';
 import { Product } from './Entities/Product';
+import { Response } from 'express';
+
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
   @Render('./User/home')
-  root(@Request() req: Request){
-    
+  root(@Res() res){
+    const user = res.locals.user;
+    return { user:user };
+  }
+  @Get('/logout')
+  @Redirect('/')
+  logout(@Res({passthrough:true}) res: Response){
+    res.clearCookie('jwt');
+    res.locals.user = null;
   }
 }
 
