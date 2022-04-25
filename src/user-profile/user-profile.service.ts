a4480a75712f3618713f07e3c21947cbe68bd2d3
teebee179 <80611478+teebee179@users.noreply.github.com>
2022-04-26 00:03:45 +0700

    Commit edit user profile

--- src/user-profile/user-profile.service.ts
+++ src/user-profile/user-profile.service.ts
@@ -1,4 +1,42 @@
 import { Injectable } from '@nestjs/common';
+import { User } from 'src/Entities/User';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class UserProfileService {}
+export class UserProfileService {
+    constructor(
+        @InjectRepository(User) private userRepo: Repository<User>,
+    ) {}
+
+    async updateUser(id: number, fullname: string, email: string, telephonenum: string, address: string): Promise<User> {
+        const updatedUser = this.userRepo.findOne({user_id: id});
+        (await updatedUser).user_fullname = fullname;
+        (await updatedUser).user_email = email;
+        (await updatedUser).user_address = address;
+        (await updatedUser).user_tel = telephonenum;
+        
+        await this.userRepo.update(id, {
+            ...((await updatedUser).user_fullname && { user_fullname: (await updatedUser).user_fullname }),
+            ...((await updatedUser).user_email && { user_email: (await updatedUser).user_email }),
+            ...((await updatedUser).user_address && { user_address: (await updatedUser).user_address }),
+            ...(((await updatedUser).user_tel && {user_tel: (await updatedUser).user_tel}))
+          });
+      
+        return this.userRepo.findOne({user_id: id});
+
+    }
+
+    async is_Exists(email: string, currEmail: string): Promise<Boolean> {
+        
+        const exists = await this.userRepo.findOne({user_email: email});
+        if(exists){
+            if(currEmail == exists.user_email){
+                return (!exists);
+            }
+            
+        }
+        
+        return (!!exists);
+    }
+}
